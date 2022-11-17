85f22d811a9c14f0e97a870f34add53e38ca471d
Rob Johansen <rob.johansen@gmail.com>
2022-11-16 20:10:36 -0700

    Add the Select component

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -3,16 +3,53 @@ import styled from 'styled-components';
 
 import { COLORS } from '../../constants';
 import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
 
 const Select = ({ label, value, onChange, children }) => {
-  const displayedValue = getDisplayedValue(value, children);
-
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <SelectContainer>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <ArrowContainer>
+        <Icon id="chevron-down" size={24} strokeWidth={2} />
+      </ArrowContainer>
+    </SelectContainer>
   );
 };
+
+const SelectContainer = styled.div`
+  position: relative;
+  width: min-content;
+`
+
+const NativeSelect = styled.select`
+  appearance: none;
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 8px;
+  color: ${COLORS.gray700};
+  font-size: ${16 / 16}rem;
+  height: 43px;
+  padding-left: 16px;
+  padding-right: 48px;
+  
+  &:hover {
+    color: ${COLORS.black};
+  }
+`
+
+const ArrowContainer = styled.div`
+  pointer-events: none;
+  position: absolute;
+  right: 10px;
+  top: 25%;
+  
+  & svg {
+     color: ${COLORS.gray700};
+  }
+  
+  ${NativeSelect}:hover + & svg {
+    color: ${COLORS.black};
+  }
+`
 
 export default Select;
